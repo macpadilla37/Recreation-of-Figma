924e10c13f6f787b37a46b8ea7dfccf5e8ce9829
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-10-17 14:02:24 -0600

    Initial Commit

---
+++ Recreation of Figma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recreation of Figma
+//
+//  Created by Mac Padilla on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recreation of Figma/Recreation_of_FigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recreation_of_FigmaApp.swift
+//  Recreation of Figma
+//
+//  Created by Mac Padilla on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Recreation_of_FigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
